f383c9fbd1463b919525e0cde4959065548ef06d
chrisnatali <chris.natali@gmail.com>
2016-03-25 10:28:57 -0400

    removing xscreensaver

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -56,7 +56,7 @@ main = do
         , modMask = mod4Mask -- make "modMask" = mod4 key
         , terminal = "urxvt"
         } `additionalKeys`
-        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock") 
+        [ ((mod4Mask .|. shiftMask, xK_z), spawn "systemctl suspend") 
         , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
         , ((0, xK_Print), spawn "scrot")
         , ((mod4Mask, xK_d), spawn "exe=`dmenu_run` && eval \"exec $exe\"")
